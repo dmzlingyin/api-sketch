5a2ccd487d46d695bca4f53e74b845ebb8eeb9df
dmzlingyin <dmzlingyin@163.com>
2024-04-01 11:45:54 +0800

    refactor: optimize code

--- main.go
+++ main.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"api-sketch/router"
+	"errors"
 	"github.com/dmzlingyin/utils/log"
 	"net/http"
 	"time"
 )
 
 func main() {
-	handler := router.Router()
 	s := &http.Server{
 		Addr:           ":8080",
-		Handler:        handler,
+		Handler:        router.Router(),
 		ReadTimeout:    5 * time.Minute,
 		WriteTimeout:   5 * time.Minute,
 		MaxHeaderBytes: 1 << 20,
 	}
-	if err := s.ListenAndServe(); err != nil {
-		log.Errorf("listen failed: %v", err)
+	if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Errorf("server serve failed: %s", err)
 	}
 }
